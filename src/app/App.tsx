8eeab9be12b18d26449056037af3128665621464
shazariustuit <shazarius@tuit.uz>
2025-08-02 12:08:09 +0500

    31 backend finished

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,8 @@
 import { classNames } from 'shared/lib/classNames/classNames';
-import { useTheme } from 'app/providers/ThemeProvider';
 import { AppRouter } from './providers/router';
 import { Navbar } from 'widgets/Navbar/ui/Navbar';
 import { Sidebar } from 'widgets/Sidebar';
-import { Suspense, useState } from 'react';
-import { Modal } from 'shared/ui/Modal/Modal';
+import { Suspense } from 'react';
 
 const App = () => {
     return (
